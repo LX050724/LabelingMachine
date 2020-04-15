518b2edb4416bdb408a203f5cdf8ac9c10668706
LX050724 <yaojizhong@outlook.com>
2020-04-15 22:07:25 +0800

    修改cmake，兼容windows

--- Src/main.cpp
+++ Src/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QTranslator *qtTranslator = new QTranslator;
+    auto *qtTranslator = new QTranslator;
     if(qtTranslator->load("./LabelingMachine_zh_CN.qm")) {
         a.installTranslator(qtTranslator);
     } else qDebug("error");
